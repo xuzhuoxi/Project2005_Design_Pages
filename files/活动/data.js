0e236c0703a651cc74251b3e7ec0808a9949eeea
xuzhuoxi <mailxuzhuoxi@163.com>
2020-05-30 03:08:41 +0800

    原型发布

---
+++ files/活动/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q,r,s,t,u,v,w,x],y,_(z,A,B,C,D,E,F,_(),G,_(H,I,J,K,L,_(M,N,O,P),Q,null,R,K,S,K,T,U,V,null,W,X,Y,Z,ba,bb,bc,X),bd,_(),be,_(),bf,_(bg,[])),bh,_(),bi,_());}; 
+var b="url",c="活动.html",d="generationDate",e=new Date(1590779276963.39),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="CorpsMax",o="CorpsCurrent",p="CorpsUnfold",q="CorpsUnfoldKeep",r="CorpsVisibleMax",s="CityShow",t="StoreOrPackage",u="TabStore",v="TabTask",w="TabHero",x="TabGuild",y="page",z="packageId",A="7de4b3cff6974e80b08d63dcdf75581d",B="type",C="Axure:Page",D="name",E="活动",F="notes",G="style",H="baseStyle",I="627587b6038d43cca051c114ac41ad32",J="pageAlignment",K="near",L="fill",M="fillType",N="solid",O="color",P=0xFFFFFFFF,Q="image",R="imageHorizontalAlignment",S="imageVerticalAlignment",T="imageRepeat",U="auto",V="favicon",W="sketchFactor",X="0",Y="colorStyle",Z="appliedColor",ba="fontName",bb="Applied Font",bc="borderWidth",bd="adaptiveStyles",be="interactionMap",bf="diagram",bg="objects",bh="masters",bi="objectPaths";
+return _creator();
+})());
